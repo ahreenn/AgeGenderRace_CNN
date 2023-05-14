128db9defbc39a5e371699f33ed83f98eca0dcde
Airin <118724561+ahreenn@users.noreply.github.com>
2023-05-14 22:17:45 +0800

    Add files via upload

---
+++ Age Model Final.py
@@ -0,0 +1,484 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+from tqdm.notebook import tqdm
+warnings.filterwarnings('ignore')
+get_ipython().run_line_magic('matplotlib', 'inline')
+from sklearn.metrics import confusion_matrix
+
+import tensorflow as tf
+from tensorflow.keras.utils import load_img
+from keras.models import Sequential, Model
+from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D, Input
+
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Conv2D, AveragePooling2D, GlobalAveragePooling2D
+from tensorflow.keras import utils
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+
+
+from sklearn.model_selection import train_test_split
+import itertools
+
+
+# In[2]:
+
+
+BASE_DIR = (r"C:\Users\Airin\Desktop\Xccelerate\Untitled Folder\Input\UTKFace")
+
+
+# In[3]:
+
+
+image_paths = []
+age_labels = []
+
+for filename in tqdm(os.listdir(BASE_DIR)):
+    try:
+        image_path = os.path.join(BASE_DIR, filename)
+        temp = filename.split('_')
+        age = int(temp[0])
+        image_paths.append(image_path)
+        age_labels.append(age)
+    
+    except ValueError:
+        pass
+    
+
+
+# In[4]:
+
+
+df = pd.DataFrame()
+df['image'], df['age'] = image_paths, age_labels
+df.head(5)
+
+
+# In[5]:
+
+
+df.shape
+
+
+# In[6]:
+
+
+train_df, test_df = train_test_split(df, test_size=0.2, random_state=25)
+
+
+# In[7]:
+
+
+print(f"No. of training examples: {train_df.shape[0]}")
+print(f"No. of testing examples: {test_df.shape[0]}")
+
+
+# # Data EDA
+
+# In[8]:
+
+
+from PIL import Image
+img = Image.open(df['image'][990])
+plt.axis('off')
+plt.imshow(img);
+
+
+# In[9]:
+
+
+sns.distplot(df['age'])
+
+
+# In[10]:
+
+
+plt.figure(figsize=(20, 20))
+files = df.iloc[0:25]
+
+for index, file, age in files.itertuples():
+    plt.subplot(5, 5, index+1)
+    img = load_img(file)
+    img = np.array(img)
+    plt.imshow(img)
+    plt.title(f"Age: {age} ")
+    plt.axis('off')
+
+
+# # Define Age Classes
+
+# In[11]:
+
+
+# Defining a function to return the class labels corresponding to the re-distributed 7 age-ranges.
+
+def class_labels_reassign(age):
+
+    if 1 <= age <= 2:
+        return 0
+    elif 3 <= age <= 9:
+        return 1
+    elif 10 <= age <= 20:
+        return 2
+    elif 21 <= age <= 27:
+        return 3
+    elif 28 <= age <= 45:
+        return 4
+    elif 46 <= age <= 65:
+        return 5
+    else:
+        return 6
+
+
+# In[12]:
+
+
+train_df['target'] = train_df['age'].map(class_labels_reassign)
+test_df['target'] = test_df['age'].map(class_labels_reassign)
+
+
+# In[13]:
+
+
+train_df.head()
+
+
+# In[14]:
+
+
+test_df.head()
+
+
+# # Organize Dataset
+
+# In[15]:
+
+
+# Converting the filenames and target class labels into lists for augmented train and test datasets.
+
+train_filenames_list = list(train_df['image'])
+train_labels_list = list(train_df['target'])
+
+test_filenames_list = list(test_df['image'])
+test_labels_list = list(test_df['target'])
+
+
+# In[16]:
+
+
+# Creating tensorflow constants of filenames and labels for augmented train and test datasets from the lists defined above.
+
+train_filenames_tensor = tf.constant(train_filenames_list)
+train_labels_tensor = tf.constant(train_labels_list)
+
+test_filenames_tensor = tf.constant(test_filenames_list)
+test_labels_tensor = tf.constant(test_labels_list)
+
+
+# # Image Greyscale Function
+
+# In[17]:
+
+
+# Defining a function to read the image, decode the image from given tensor and one-hot encode the image label class.
+# Changing the channels para in tf.io.decode_jpeg from 3 to 1 changes the output images from RGB coloured to grayscale.
+
+num_classes = 7
+
+def _parse_function(image, label):
+    
+    image_string = tf.io.read_file(image)
+    image_decoded = tf.io.decode_jpeg(image_string, channels=1)    # channels=1 to convert to grayscale, channels=3 to convert to RGB.
+    # image_resized = tf.image.resize(image_decoded, [200, 200])
+    label = tf.one_hot(label, num_classes)
+
+    return image_decoded, label
+
+
+# In[18]:
+
+
+# Getting the dataset ready for the neural network.
+# Using the tensor vectors defined above, accessing the images in the dataset and passing them through the function defined above.
+
+train_dataset = tf.data.Dataset.from_tensor_slices((train_filenames_tensor, train_labels_tensor))
+train_dataset = train_dataset.map(_parse_function)
+# train_aug_dataset = train_aug_dataset.repeat(3)
+train_dataset = train_dataset.batch(256)    # Same as batch_size hyperparameter in model.fit() below.
+
+test_dataset = tf.data.Dataset.from_tensor_slices((test_filenames_tensor, test_labels_tensor))
+test_dataset = test_dataset.map(_parse_function)
+# test_dataset = test_dataset.repeat(3)
+test_dataset = test_dataset.batch(256)    # Same as batch_size hyperparameter in model.fit() below.
+
+
+# # CNN Architecture
+
+# In[19]:
+
+
+# Defining the architecture of the sequential neural network.
+
+final_cnn = Sequential()
+
+
+# In[20]:
+
+
+# Input layer with 32 filters, followed by an AveragePooling2D layer.
+final_cnn.add(Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=(200, 200, 1)))    # 3rd dim = 1 for grayscale images.
+final_cnn.add(AveragePooling2D(pool_size=(2,2)))
+
+
+# In[21]:
+
+
+# Three Conv2D layers with filters increasing by a factor of 2 for every successive Conv2D layer.
+final_cnn.add(Conv2D(filters=64, kernel_size=3, activation='relu'))
+final_cnn.add(AveragePooling2D(pool_size=(2,2)))
+
+final_cnn.add(Conv2D(filters=128, kernel_size=3, activation='relu'))
+final_cnn.add(AveragePooling2D(pool_size=(2,2)))
+
+final_cnn.add(Conv2D(filters=256, kernel_size=3, activation='relu'))
+final_cnn.add(AveragePooling2D(pool_size=(2,2)))
+
+
+# In[22]:
+
+
+# A GlobalAveragePooling2D layer before going into Dense layers below.
+# GlobalAveragePooling2D layer gives no. of outputs equal to no. of filters in last Conv2D layer above (256).
+final_cnn.add(GlobalAveragePooling2D())
+
+
+# In[23]:
+
+
+# One Dense layer with 132 nodes so as to taper down the no. of nodes from no. of outputs of GlobalAveragePooling2D layer above towards no. of nodes in output layer below (7).
+final_cnn.add(Dense(132, activation='relu'))
+
+
+# In[24]:
+
+
+# Output layer with 7 nodes (equal to the no. of classes).
+final_cnn.add(Dense(7, activation='softmax'))
+
+
+# In[25]:
+
+
+final_cnn.summary()
+
+
+# In[26]:
+
+
+# Compiling the above created CNN architecture.
+
+final_cnn.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+# In[27]:
+
+
+# Creating a TensorBoard callback object and saving it at the desired location.
+
+tensorboard = TensorBoard(log_dir=f"age_output/Output/cnn_logs")
+
+
+# In[28]:
+
+
+# Creating a ModelCheckpoint callback object to save the model according to the value of val_accuracy.
+
+checkpoint = ModelCheckpoint(filepath=f"age_output/Output/cnn_logs/age_model_2_checkpoint.h5",
+                             monitor='val_accuracy',
+                             save_best_only=True,
+                             save_weights_only=False,
+                             verbose=1
+                            )
+
+
+# # Model Fitting
+
+# In[29]:
+
+
+# Fitting the above created CNN model.
+
+final_cnn_history = final_cnn.fit(train_dataset,
+                                  batch_size=256,
+                                  validation_data=test_dataset,
+                                  epochs=60,
+                                  callbacks=[tensorboard, checkpoint],
+                                  shuffle=False    # shuffle=False to reduce randomness and increase reproducibility
+                                 )
+
+
+# # Checking Model Performance
+
+# In[30]:
+
+
+# Checking the train and test loss and accuracy values from the neural network above.
+
+train_loss = final_cnn_history.history['loss']
+test_loss = final_cnn_history.history['val_loss']
+train_accuracy = final_cnn_history.history['accuracy']
+test_accuracy = final_cnn_history.history['val_accuracy']
+
+
+# In[35]:
+
+
+# Plotting a line chart to visualize the loss and accuracy values by epochs.
+
+fig, ax = plt.subplots(ncols=2, figsize=(15,7))
+
+ax = ax.ravel()
+
+ax[0].plot(train_loss, label='Train Loss', color='royalblue', marker='o', markersize=5)
+ax[0].plot(test_loss, label='Test Loss', color = 'orangered', marker='o', markersize=5)
+
+ax[0].set_xlabel('Epochs', fontsize=14)
+ax[0].set_ylabel('Categorical Crossentropy', fontsize=14)
+
+ax[0].legend(fontsize=14)
+ax[0].tick_params(axis='both', labelsize=12)
+
+ax[1].plot(train_accuracy, label='Train Accuracy', color='royalblue', marker='o', markersize=5)
+ax[1].plot(test_accuracy, label='Test Accuracy', color='orangered', marker='o', markersize=5)
+
+ax[1].set_xlabel('Epochs', fontsize=14)
+ax[1].set_ylabel('Accuracy', fontsize=14)
+
+ax[1].legend(fontsize=14)
+ax[1].tick_params(axis='both', labelsize=12)
+
+fig.suptitle(x=0.5, y=0.92, t="Lineplots showing loss and accuracy of CNN model by epochs", fontsize=16)
+
+plt.savefig("age_output/Output/cnn_logs/final_cnn_loss_accuracy.png")
+
+
+# In[36]:
+
+
+# Evaluating the model on test dataset.
+
+final_cnn_score = final_cnn.evaluate(test_dataset, verbose=1)
+
+
+# In[37]:
+
+
+# Printing the relevant score summary.
+
+final_cnn_labels = final_cnn.metrics_names
+print(f'CNN model {final_cnn_labels[0]} \t\t= {round(final_cnn_score[0], 3)}')
+print(f'CNN model {final_cnn_labels[1]} \t= {round(final_cnn_score[1], 3)}')
+
+
+# In[38]:
+
+
+# Saving the model as a h5 file for possible use later.
+
+final_cnn.save(f"Age_input_output/Output/cnn_logs/age_model_acc_{round(final_cnn_score[1], 3)}.h5", save_format='h5')
+
+
+# # Plotting Confusion Matrix
+
+# In[39]:
+
+
+# Generating predictions from the model above.
+
+final_cnn_pred = final_cnn.predict(test_dataset)
+final_cnn_pred = final_cnn_pred.argmax(axis=-1)
+
+
+# In[40]:
+
+
+# Generating a confusion matrix based on above predictions.
+
+conf_mat = confusion_matrix(test_labels_list, final_cnn_pred)
+conf_mat
+
+
+# In[41]:
+
+
+# Defining a function to plot the confusion matrix in a grid for easier visualization.
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion Matrix', export_as='confusion_matrix', cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    # print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title, fontsize=16)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True labels', fontsize=14)
+    plt.xlabel('Predicted labels', fontsize=14)
+
+
+# In[42]:
+
+
+# Plotting the confusion matrix using the function defined above.
+
+cm_plot_labels = ['1-2', '3-9', '10-20', '21-27', '28-45', '46-65', '66-116']
+
+plt.figure(figsize=(16,8))
+plot_confusion_matrix(conf_mat, cm_plot_labels, normalize=True,
+                      title="Confusion Matrix based on predictions from CNN model",
+                      export_as="final_cnn_conf_mat_norm"
+                     )
+
+plt.show()
+
+
+# In[43]:
+
+
+final_cnn.save('age_output/Output/cnn_logs/age_model_final_save.h5')
+
+
+# In[ ]:
+
+
+
+
---
+++ Predict Model.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+import cv2
+
+import tensorflow as tf
+
+#the following are to do with this interactive notebook code
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+from matplotlib import pyplot as plt # this lets you draw inline pictures in the notebooks
+import pylab # this allows you to control figure size 
+pylab.rcParams['figure.figsize'] = (10.0, 8.0) # this controls figure size in the notebook
+
+
+# # Loading Age/Gender/Race Models
+
+# In[6]:
+
+
+age_model = load_model(r"C:\Users\Airin\Desktop\Xccelerate\Untitled Folder\age_output\Output\cnn_logs\age_model_final_save.h5")
+
+# summarize model.
+age_model.summary()
+
+
+# In[7]:
+
+
+gender_model = load_model(r"C:\Users\Airin\Desktop\Xccelerate\Untitled Folder\gender_output\cnn_logs\gender_model.h5")
+
+# summarize model.
+gender_model.summary()
+
+
+# In[165]:
+
+
+race_model = load_model(r"C:\Users\Airin\Desktop\Xccelerate\Untitled Folder\race_output\cnn_logs\race_model_final_saveA.h5")
+# summarize model.
+race_model.summary()
+
+
+# In[166]:
+
+
+# Labels on Age, Gender and Race to be predicted
+
+age_ranges = ['1-2', '3-9', '10-20', '21-27', '28-45', '46-65', '66-116']
+gender_ranges = ['male', 'female']
+race_ranges= ['white','black','asian', 'indian', 'others']
+
+
+# # Loading Test Images
+
+# In[178]:
+
+
+# Song Hye Kyo - Female - Asian - 41 years old
+#img_path = (r"C:\Users\Airin\Desktop\Xccelerate\Untitled Folder\test_input\images\Screenshot 2023-05-13 164811.jpg")
+
+# Daniel Kaluuya - Male - Black - 34 years old
+#img_path = (r"C:\Users\Airin\Desktop\Xccelerate\Untitled Folder\test_input\images\Screenshot 2023-05-13 171159.jpg")
+
+# Jenna Ortega - Female - White - 20 years old
+#img_path = (r"C:\Users\Airin\Desktop\Xccelerate\Untitled Folder\test_input\images\Screenshot 2023-05-13 165620.jpg")
+
+# Antonia Gentry - Female - Biracial (White/Black) - 25 years old
+#img_path = (r"C:\Users\Airin\Desktop\Xccelerate\Untitled Folder\test_input\images\Screenshot 2023-05-13 165731.jpg")
+
+# Priyanka Chopra - Female - Indian - 40 years old
+#img_path = (r"C:\Users\Airin\Desktop\Xccelerate\Untitled Folder\test_input\images\priyanka_chopra.jpg")
+
+# Pair photo
+img_path = (r"C:\Users\Airin\Desktop\Xccelerate\Untitled Folder\test_input\images\Screenshot 2023-05-13 170357.jpg")
+
+# Group photo
+#img_path = (r"C:\Users\Airin\Desktop\Xccelerate\Untitled Folder\test_input\images\Screenshot 2023-05-13 170049.jpg")
+
+
+# In[179]:
+
+
+from IPython.display import Image 
+pil_img = Image(filename=img_path)
+display(pil_img)
+
+
+# # Results on Age/Gender/Race
+
+# In[180]:
+
+
+tf.debugging.disable_traceback_filtering
+
+
+# In[181]:
+
+
+test_image = cv2.imread(img_path)
+gray = cv2.cvtColor(test_image,cv2.COLOR_BGR2GRAY)
+face_cascade = cv2.CascadeClassifier(r"C:\Users\Airin\Desktop\Xccelerate\Untitled Folder\test_input\cv2_cascade_classifier\haarcascade_frontalface_default.xml")
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+i = 0
+
+for (x,y,w,h) in faces:
+    i = i+1
+    cv2.rectangle(test_image,(x,y),(x+w,y+h),(203,12,255),2)
+
+    img_gray=gray[y:y+h,x:x+w]
+  
+    gender_img = cv2.resize(img_gray, (100, 100), interpolation = cv2.INTER_AREA)
+    gender_image_array = np.array(gender_img)
+    gender_input = np.expand_dims(gender_image_array, axis=0)
+    output_gender=gender_ranges[np.argmax(gender_model.predict(gender_input))]
+
+    age_image=cv2.resize(img_gray, (200, 200), interpolation = cv2.INTER_AREA)
+    age_input = age_image.reshape(-1, 200, 200, 1)
+    output_age = age_ranges[np.argmax(age_model.predict(age_input))]
+    
+    race_img = cv2.resize(img_gray, (100, 100), interpolation = cv2.INTER_AREA)
+    race_image_array = np.array(race_img)
+    race_input = np.expand_dims(race_image_array, axis=0)
+    output_race= race_ranges[np.argmax(race_model.predict(race_input))]
+    
+    output_str = str(i) + ": "+  output_gender + ', '+ output_age + ', '+ output_race 
+    print(output_str)
+  
+    col = (0,255,0)
+
+    cv2.putText(test_image, str(i),(x,y),cv2.FONT_HERSHEY_SIMPLEX,1,col,2)
+
+plt.imshow(cv2.cvtColor(test_image, cv2.COLOR_BGR2RGB))
+
+
+# In[ ]:
+
+
+
+
